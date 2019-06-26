1122ad5bdebf5a5575f70e7fcb3c5d280e0fabf5
Akshay Suresh <akshay_s@rollingarrays.com>
2019-06-27 01:57:17 +0530

    FIXED: removed eslint rule no param reassign | moved login and register to vuex

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import router from './router'
+import router from './router';
 
 import axios from './axios-auth';
 import api from './api-endpoints';
@@ -12,19 +12,26 @@ export default new Vuex.Store({
   state: {
     token: null,
     userId: null,
+    user: null,
   },
   getters: {
 
   },
   mutations: {
-
+    authUser(state, userData) {
+      state.token = userData.token;
+      state.userId = userData.userId;
+      state.user = userData.user;
+    },
   },
   actions: {
     register({ commit }, authData) {
       axios
         .post(api.register, authData)
-        .then(() => {
-          router.push({ name: 'login' });
+        .then((response) => {
+          if (response.status === 201) {
+            router.push({ name: 'login' });
+          }
         })
         .catch(() => {
         });
@@ -33,8 +40,15 @@ export default new Vuex.Store({
     login({ commit }, authData) {
       axios
         .post(api.login, authData)
-        .then((reponse) => {
-          // console.log(reponse);
+        .then((response) => {
+          if (response.status === 200) {
+            console.log('here', response.data);
+            commit('authUser', {
+              token: response.data.token,
+              userId: response.data.user.id,
+              user: response.data.user,
+            });
+          }
         })
         .catch(() => {
         });
